ec876a48c6db1482d5a2622432a02375833d29fd
Anthony Gitter <agitter@users.noreply.github.com>
2022-09-27 22:44:17 -0500

    Add third argument to makeConsensus

--- fall2022/hw1/makeConsensus.sh
+++ fall2022/hw1/makeConsensus.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 # Python
-python ./makeConsensus.py "$1" "$2"
+python ./makeConsensus.py "$1" "$2" $3
 
 # $1: first argument [graph_list.txt]
 # $2: second argument [consensus_graph.txt]
+# $3: third argument [edge_count]
